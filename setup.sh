d3097d385ee93d46cf09a9489fccb7f22f2a82cc
Dan <danhmcinerney@gmail.com>
2017-12-21 19:59:04 -0700

    took out the apt-get remove bc marcello told me to

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
-echo "\n[*] Running: apt-get install python3-dev python-pip libssl-dev -y"
-apt-get install python3-dev python-pip libssl-dev -y
+echo "\n[*] Running: apt-get install python3-dev python-pip libssl-dev smbclient -y"
+apt-get install python3-dev python-pip libssl-dev smbclient -y
 echo "\n[*] Running: pip install pipenv"
 pip install pipenv
 echo "\n[*] Running: git submodule init"
@@ -8,8 +8,6 @@ echo "\n[*] Running: git submodule update --recursive"
 git submodule update --recursive
 echo "\n[*] Running: cd submodules/JohnTheRipper/src && ./configure && make"
 cd submodules/JohnTheRipper/src && ./configure && make
-echo "\n[*] Running: apt-get remove python-impacket"
-apt-get remove python-impacket
 echo "\n[*] Running: cd ../../impacket/"
 cd ../../impacket/
 echo "\n[*] Running: python2 setup.py install"
@@ -18,5 +16,7 @@ echo "\n[*] Running: pip2 install ldap3"
 pip2 install ldap3
 echo "\n[*] Running: pipenv install --three"
 pipenv install --three
-echo '[*] Run "pipenv shell" to enter virtualenv then run icebreaker, e.g., ./icebreaker.py -l targets.txt'
+echo '\n[*] KALI USERS: run "apt-get remove python-impacket" before running icebreaker'
+echo '\n[*] Run "pipenv shell" before running icebreaker'
+echo '\n[*] Example usage: ./icebreaker.py -l targets.txt'
 
