76c970adf22a86b26851c2598f56649f4083a541
root <root@localhost.localdomain>
2018-05-14 17:30:04 -0600

    started dockersetup

--- docker-setup.sh
+++ docker-setup.sh
@@ -6,14 +6,14 @@ apt-get update
 # pipenv will fail to build virtualenv if Python or the environment or something is installed weird because 
 # pew.py will attempt to import a backports function and fail even though the module is installed right in pip
 # but if we just apt-get install python-backports-shutil-get-terminal-size then that solves the problem apparently
-echo -e '\n[*] Running: apt-get install python3-dev python-pip smbclient xterm python-backports-shutil-get-terminal-size -y'
-apt-get install make sudo python3-dev tmux python-pip smbclient xterm python-backports-shutil-get-terminal-size build-essential -y
+echo -e '\n[*] Running: apt-get install python3 python python-dev lsb_release python3-dev python-pip smbclient xterm python-backports-shutil-get-terminal-size -y'
+apt-get install libicu55 libxml2-dev lib sudo python3-dev tmux python-pip smbclient xterm libssl-dev python-backports-shutil-get-terminal-size build-essential -y
 
 echo -e '\n[*] Running: pip2 install pexpect mitm6 ldap3'
-pip2 install --upgrade mitm6 pexpect ldap3
+pip2 install --upgrade mitm6 m2crypto pexpect ldap3
 
-echo -e '\n[*] Running: rm submodules/Responder/Responder.db'
-rm submodules/Responder/Responder.db
+#echo -e '\n[*] Running: rm submodules/Responder/Responder.db'
+#rm submodules/Responder/Responder.db
 
 echo -e '\n[*] Running: git submodule update  --init --recursive -j 7'
 git submodule update --init --recursive -j 7
@@ -39,8 +39,3 @@ yes | ./install.sh
 
 echo -e '\n[*] Running: cd ../../../'
 cd ../../../
-
-#echo -e '\n[*] KALI USERS: run "apt-get remove python-impacket" before running icebreaker'
-echo -e '[*] Run "pipenv install --three"'
-echo -e '[*] Run "pipenv shell" before running icebreaker'
-echo -e '[*] Example usage: ./icebreaker.py -l targets.txt'
